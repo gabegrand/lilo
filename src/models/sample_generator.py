39e73443cd05c89b3cf85727f348bced7bcbe900
Gabe Grand <grandg@mit.edu>
2022-03-04 18:03:40 -0500

    Finished CodexSampleGenerator loop

--- src/models/sample_generator.py
+++ src/models/sample_generator.py
@@ -4,6 +4,7 @@ sample_generator.py | Author: Gabe Grand.
 Queries Codex to generate new samples based on existing samples.
 
 """
+import json
 import os
 
 import numpy as np
@@ -25,6 +26,8 @@ ModelRegistry = model_loaders.ModelLoaderRegistries[model_loaders.SAMPLE_GENERAT
 class CodexSampleGenerator(model_loaders.ModelLoader):
     name = "codex_sample_generator"
 
+    query_results_file = "codex_query_results.json"
+
     ENGINE = "davinci-codex"
     SEP = "\n"
 
@@ -36,10 +39,27 @@ class CodexSampleGenerator(model_loaders.ModelLoader):
         super().__init__()
 
     def generate_samples(
-        self, experiment_state, task_splits, task_ids_in_splits, debug=False
+        self,
+        experiment_state,
+        task_splits,
+        task_ids_in_splits,
+        n_samples: int = 5,
+        n_train_programs_per_prompt: int = 10,
+        debug: bool = False,
     ):
         """
         Queries Codex API to generate new samples.
+
+        params:
+            experiment_state: experiment_state
+            task_splits: task_splits
+            task_ids_in_splits: task_ids_in_splits
+            n_samples: Number of programs for Codex to generate
+            n_train_programs_per_prompt: Number of training programs to include
+                in the Codex prompt.
+            debug: If True, replaces live query to Codex with a random sample
+                from the training set.
+
         """
         frontiers = experiment_state.get_frontiers_for_ids(TRAIN, ALL)
 
@@ -53,23 +73,41 @@ class CodexSampleGenerator(model_loaders.ModelLoader):
             return None
 
         # TODO(gg): Prevent generation of duplicate programs
-        set(map(hash, programs_train))
+        # programs_train_hashes = set(map(hash, programs_train))
 
-        programs_for_prompt = np.random.choice(programs_train, size=5, replace=False)
-        prompt = self.SEP.join(programs_for_prompt) + self.SEP
+        programs_for_prompt = list(
+            np.random.choice(
+                programs_train,
+                size=min(n_train_programs_per_prompt, len(programs_train)),
+                replace=False,
+            )
+        )
+        prompt_text = self.SEP.join(programs_for_prompt) + self.SEP
 
+        print(f"Querying Codex with prompt ({len(programs_for_prompt)} examples)...")
         if debug:
-            completion = self.query_mock(experiment_state)
+            completion = self.query_mock(experiment_state, n_samples=n_samples)
         else:
-            completion = self.query_codex(prompt)
+            completion = self.query_codex(prompt_text, n_samples=n_samples)
 
         if completion is not None:
+            query_results = {
+                "programs_valid": [],
+                "programs_invalid": [],
+                "prompt_text": prompt_text,
+                "prompt_programs": programs_for_prompt,
+                "separator": self.SEP,
+                "completion": completion.to_dict_recursive(),
+            }
+
             for choice in completion["choices"]:
                 program_str = choice["text"]
                 try:
                     p = Program.parse(program_str)
+                    query_results["programs_valid"].append(program_str)
                 except:
-                    print(f"Failed to parse Codex-generated program: {program_str}")
+                    # print(f"Failed to parse Codex-generated program: {program_str}")
+                    query_results["programs_invalid"].append(program_str)
                     continue
 
                 # TODO(gg): avoid adding duplicate generated programs
@@ -95,11 +133,23 @@ class CodexSampleGenerator(model_loaders.ModelLoader):
 
                 experiment_state.sample_tasks[TRAIN].append(task)
                 experiment_state.sample_frontiers[TRAIN][task] = frontier
+
+            query_results_filepath = os.path.join(
+                os.getcwd(),
+                experiment_state.get_checkpoint_directory(),
+                self.query_results_file,
+            )
+            with open(query_results_filepath, "w") as f:
+                json.dump(query_results, f)
+            print(
+                f"Codex query results:\nVALID: {len(query_results['programs_valid'])}\nINVALID: {len(query_results['programs_invalid'])}"
+            )
+            print(f"Wrote results: {query_results_filepath}")
         else:
             # TODO(gg): Better error handling
-            pass
+            print("Query to Codex encountered an error. No samples were added.")
 
-    def query_codex(self, prompt: str, n_samples: int = 3, temperature: int = 0.75):
+    def query_codex(self, prompt: str, n_samples: int, temperature: float = 0.75):
         """
         TODO(gg): Make params for temperature, n, and max_tokens
         """
