396d3c17c065c1e3073f472058c703311a15d2d3
Gabe Grand <grandg@mit.edu>
2022-03-11 11:09:17 -0500

    EtaLongVisitor

--- src/models/stitch_rewriter.py
+++ src/models/stitch_rewriter.py
@@ -9,7 +9,7 @@ import json
 
 import src.models.model_loaders as model_loaders
 from dreamcoder.frontier import Frontier, FrontierEntry
-from dreamcoder.program import Program
+from dreamcoder.program import EtaLongVisitor, Program
 from src.models.stitch_base import StitchBase
 
 ModelRegistry = model_loaders.ModelLoaderRegistries[model_loaders.PROGRAM_REWRITER]
@@ -91,6 +91,8 @@ class StitchProgramRewriter(StitchBase, model_loaders.ModelLoader):
                         program_data["program"], inv_name_to_dc_fmt
                     )
                     p = Program.parse(p_str)
+                    # Hack to avoid fatal error when computing likelihood summaries
+                    p = EtaLongVisitor(request=task.request).execute(p)
                     frontier_rewritten.entries.append(
                         FrontierEntry(
                             program=p,
