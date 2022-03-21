95ef8c43788f89b3d0e334785285c87e2e052cb5
Gabe Grand <grandg@mit.edu>
2022-03-21 16:26:05 -0400

    Add run_iterative_experiment.py

---
+++ run_iterative_experiment.py
@@ -0,0 +1,20 @@
+from run_experiment import *
+
+
+def main(args):
+    for global_batch_size in [10, 20, 30]:
+        config = load_config_from_file(args)
+        config["experiment_iterator"]["task_batcher"]["params"][
+            "global_batch_size"
+        ] = global_batch_size
+        config["metadata"]["experiment_id"] += f"_{global_batch_size}"
+
+        experiment_state, experiment_iterator = init_experiment_state_and_iterator(
+            args, config
+        )
+        run_experiment(args, experiment_state, experiment_iterator)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
